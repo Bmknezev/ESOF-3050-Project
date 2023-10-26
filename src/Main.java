68550b0d3e55550f526d1e9fb666e87d978ae3f9
braydon <bmknezev@lakeheadu.ca>
2023-10-26 13:23:54 -0400

    added OCFS files

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 
 
+
+
 public class Main {
     public static void main(String[] args) {
 
+    msg p = new msg(3);
 
 
     }
---
+++ src/SmartHomeClient.java
@@ -0,0 +1,17 @@
+public class SmartHomeClient extends AbstractClient{
+
+    /**
+     * Constructs the client.
+     *
+     * @param host the server's host name.
+     * @param port the port number.
+     */
+    public SmartHomeClient(String host, int port) {
+        super(host, port);
+    }
+
+    @Override
+    protected void handleMessageFromServer(Object msg) {
+
+    }
+}
