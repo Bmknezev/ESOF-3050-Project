bbf6d8d7e6f442855d9cbcb50d40adc35f58968b
braydon <bmknezev@lakeheadu.ca>
2023-11-01 09:28:34 -0400

    can now send objects instead of just messages

--- src/Main.java
+++ src/Main.java
@@ -7,23 +7,29 @@ public class Main {
         SmartHomeClient s = new SmartHomeClient("127.0.0.1", 19920);
         java.util.Scanner scanner = new java.util.Scanner(System.in);
 
+        //create new light object
+        light l = new light(100, 0.5f);
+
         try {
             s.openConnection();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        String msg = "connection established";
-
-        while(!Objects.equals(msg, "/close")) {
-            msg = scanner.nextLine();
-            try {
-                s.sendToServer(msg);
-            } catch (IOException e) {
-                    System.out.println("not connected to server");
-            }
-
+        try{
+            s.sendToServer(l);
+            System.out.println("Message 1 sent");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+
+    try{
+        s.sendToServer("Hello");
+        System.out.println("Message 2 sent");
+    } catch (IOException e) {
+        throw new RuntimeException(e);
+    }
+
         try {
             s.closeConnection();
         } catch (IOException e) {
