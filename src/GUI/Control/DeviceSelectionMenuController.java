b897f58b32198f8145de65aa87be620dd2c768b1
Francisco <fcraposo@lakeheadu.ca>
2023-11-12 17:48:05 -0500

    Added device menu & server connection toggle

---
+++ src/GUI/Control/DeviceSelectionMenuController.java
@@ -0,0 +1,11 @@
+package GUI.Control;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
+public class DeviceSelectionMenuController {
+
+    @FXML
+    private GridPane deviceGridPane;
+
+}
