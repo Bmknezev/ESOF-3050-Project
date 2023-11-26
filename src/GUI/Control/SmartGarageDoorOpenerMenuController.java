890a902633078a053fd8ad8b675335e1e0c4cca0
braydon <bmknezev@lakeheadu.ca>
2023-11-25 21:34:03 -0500

    can now change garage door pin

--- src/GUI/Control/SmartGarageDoorOpenerMenuController.java
+++ src/GUI/Control/SmartGarageDoorOpenerMenuController.java
@@ -6,12 +6,13 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import messages.AbstractDeviceMessage;
+import messages.PinMessage;
 import messages.server.GarageDoorMessage;
 
 public class SmartGarageDoorOpenerMenuController extends AbstractDeviceController implements Updatable {
@@ -51,6 +52,10 @@ public class SmartGarageDoorOpenerMenuController extends AbstractDeviceControlle
 
     private int deviceID;
 
+    private TextInputDialog td = new TextInputDialog("");
+
+    private int pin;
+
 
     public void setPreviousScene(Scene previousScene) {
         previous = previousScene;
@@ -64,25 +69,96 @@ public class SmartGarageDoorOpenerMenuController extends AbstractDeviceControlle
 
     @Override
     public void update(AbstractDeviceMessage msg) {
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                GarageDoorMessage message = (GarageDoorMessage) msg;
-                SmartDeviceNameLabel.setText(message.getName());
-                if (message.getDoorStatus()) {
-                    StatusIndicatorLabel.setText("Open");
-                } else {
-                    StatusIndicatorLabel.setText("Closed");
-                }
-
-
+        Platform.runLater(() -> {
+            GarageDoorMessage message = (GarageDoorMessage) msg;
+            SmartDeviceNameLabel.setText(message.getName());
+            System.out.println(message.getDoorStatus());
+            if (message.getDoorStatus()) {
+                StatusIndicatorLabel.setText("Open");
+            } else {
+                StatusIndicatorLabel.setText("Closed");
             }
+            deviceID = message.getDeviceID();
+            pin = message.getPIN();
+
+
         });
 
     }
 
     private void UpdateServer(){
-        GarageDoorMessage msg = new GarageDoorMessage(deviceID, SmartDeviceNameLabel.getText(), false, 0);
+        boolean status = StatusIndicatorLabel.getText().equals("Open");
+        GarageDoorMessage msg = new GarageDoorMessage(deviceID, SmartDeviceNameLabel.getText(), status, true, pin);
         client.UpdateServer(msg);
+    }
+
+    public void ToggleGarageDoorButtonPressed(ActionEvent actionEvent) {
+        if (StatusIndicatorLabel.getText().equals("Closed")) {
+            td.setHeaderText("Enter PIN");
+            td.setTitle("Enter PIN");
+            td.getEditor().setText("");
+            DialogPane dialogPane = td.getDialogPane();
+            Label pinPrompt = new Label("Enter PIN");
+            TextField pinInput = new TextField();
+            dialogPane.setContent(new VBox(8, new HBox(8, pinPrompt, pinInput)));
+
+            td.setResultConverter(dialogButton -> {
+                if (dialogButton == ButtonType.OK) {
+                    client.checkPIN(new PinMessage(deviceID, Integer.parseInt(pinInput.getText()), -1, true), this);
+                }
+                return null;
+            });
+
+            td.showAndWait();
+
+
+        } else {
+            StatusIndicatorLabel.setText("Closed");
+            SmartDeviceImageView.setImage(new javafx.scene.image.Image("/GUI/Images/Garage Door Opener Icon.png"));
+            UpdateServer();
+        }
+    }
+
+    @Override
+    public void response(PinMessage msg) {
+        System.out.println("PIN response received22" + msg.getPinStatus());
+        Platform.runLater(() -> {
+            if (msg.getPinStatus()) {
+                StatusIndicatorLabel.setText("Open");
+                SmartDeviceImageView.setImage(new javafx.scene.image.Image("/GUI/Images/Garage Door Opener Icon.png"));
+                UpdateServer();
+            } else {
+                td.setHeaderText("Wrong PIN entered");
+                td.setTitle("Error");
+                DialogPane dialogPane = td.getDialogPane();
+                dialogPane.setContent(new VBox());
+
+                td.showAndWait();
+            }
+        });
+    }
+
+    public void ChangePINButtonPressed(ActionEvent actionEvent) {
+        DialogPane dialogPane = td.getDialogPane();
+        Label pinPrompt = new Label("Enter current PIN");
+        TextField pinInput = new TextField();
+        Label pinPrompt2 = new Label("Enter new PIN");
+        TextField pinInput2 = new TextField();
+        dialogPane.setContent(new VBox(8, new HBox(8, pinPrompt, pinInput), new HBox(8, pinPrompt2, pinInput2)));
+        td.setHeaderText("Change PIN");
+        td.setTitle("Change PIN");
+        td.setResultConverter(dialogButton -> {
+            if (dialogButton == ButtonType.OK) {
+                client.checkPIN(new PinMessage (deviceID, Integer.parseInt(pinInput.getText()), Integer.parseInt(pinInput2.getText()), false), this);
+            }
+            return null;
+        });
+
+        td.showAndWait();
+    }
+
+    @Override
+    public void setPIN(int newPin) {
+        pin = newPin;
     }
 }
